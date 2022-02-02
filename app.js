fa436b106fa62ec4fb6c29dfbee375583143aabf
rawand1998 <rawandgaradh@gmail.com>
2022-02-02 16:41:53 +0200

    install express and some package

---
+++ app.js
@@ -0,0 +1,14 @@
+require('env2')('.env');
+const { join } = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const router = require('./routes');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 4000);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/v1', router);
+module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
+const app = require('./app');
 
+app.listen(app.get('PORT'), () => {
+  console.log(`Server in listening at http://localhost:${app.get('PORT')}`);
+});
 
 
 
